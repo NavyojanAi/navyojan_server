5db7b277488392d777fd1e68b5de60fd327c139b
noneotherthanmodi <uditmodi03@gmail.com>
2024-07-25 21:02:11 +0530

    backend

--- navyojan/settings.py
+++ navyojan/settings.py
@@ -14,15 +14,26 @@ from pathlib import Path
 import os 
 import firebase_admin
 from firebase_admin import credentials
-
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
-FIREBASE_CREDENTIALS = credentials.Certificate(
-    os.path.join(BASE_DIR, 'D:/navyojan_backend/navyojanai-bee80-firebase-adminsdk-8hn6x-7c948a12f3.json'))
+FIREBASE_CREDENTIALS = credentials.Certificate({
+    "type": os.getenv('FIREBASE_TYPE'),
+    "project_id": os.getenv('FIREBASE_PROJECT_ID'),
+    "private_key_id": os.getenv('FIREBASE_PRIVATE_KEY_ID'),
+    "private_key": os.getenv('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
+    "client_email": os.getenv('FIREBASE_CLIENT_EMAIL'),
+    "client_id": os.getenv('FIREBASE_CLIENT_ID'),
+    "auth_uri": os.getenv('FIREBASE_AUTH_URI'),
+    "token_uri": os.getenv('FIREBASE_TOKEN_URI'),
+    "auth_provider_x509_cert_url": os.getenv('FIREBASE_AUTH_PROVIDER_X509_CERT_URL'),
+    "client_x509_cert_url": os.getenv('FIREBASE_CLIENT_X509_CERT_URL')
+})
 
 firebase_admin.initialize_app(FIREBASE_CREDENTIALS)
 
