6c5d2881478895fda7744ec321967dbcf372284a
noneotherthanmodi <uditmodi03@gmail.com>
2024-07-30 23:38:57 +0530

    added scrapping to the backend

--- navyojan/settings.py
+++ navyojan/settings.py
@@ -99,10 +99,19 @@ WSGI_APPLICATION = 'navyojan.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
+#TO CONNECT TO THE DATABASE: RUN THE FOLLOWING COMMANDS IN THE TERMINAL
+# psql -U postgres
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'navyojan',
+        'USER': 'navy',
+        'PASSWORD': 'navy',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
