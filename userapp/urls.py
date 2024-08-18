02459792fe27d40410771236ecc6cbcb5c0400c1
sandeepkota312 <sandeepkota312@gmail.com>
2024-08-18 11:00:25 +0530

    comments-2

--- userapp/urls.py
+++ userapp/urls.py
@@ -6,7 +6,6 @@ from rest_framework.routers import DefaultRouter
 from userapp.views import UserProfileViewSet, ScholarshipDataViewSet, GenerateOTP, VerifyOTP, CategoryViewSet,UserScholarshipDataViewset
 
 from userapp.views import UserProfileViewSet, ScholarshipDataViewSet, GenerateOTP, VerifyOTP
-# from userapp.views.scholarships import ScholarshipListAPIView
 
 router = DefaultRouter()
 router.register(r'profiles', UserProfileViewSet)
@@ -18,5 +17,4 @@ urlpatterns = [
     path('', include(router.urls)),
     path('profiles/generate-otp/', GenerateOTP.as_view(), name='generate-otp'),
     path('profiles/verify-otp/', VerifyOTP.as_view(), name='verify-otp'),
-    # path('scholarships/', ScholarshipListAPIView.as_view(), name='scholarship-list'),
 ]
