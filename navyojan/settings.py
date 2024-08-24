0c4b935263f24d3ce5cbd8542c41679e96b780f0
divyej <divj32459@gmail.com>
2024-08-24 21:21:48 +0530

    chore: Update ALLOWED_HOSTS in settings.py

--- navyojan/settings.py
+++ navyojan/settings.py
@@ -56,7 +56,13 @@ SECRET_KEY = 'django-insecure-x$im$wy@jq*%&rjoccud*3%m9w)qo6vug+a@50v@y7##598q@+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '159.65.158.135',  # Your server's IP address
+    'localhost',
+    '127.0.0.1',
+    # Add any other hosts or domain names here
+]
+
 
 
 # Application definition
