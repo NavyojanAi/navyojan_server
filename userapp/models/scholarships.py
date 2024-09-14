589536db2fed4947a6e0451b548b47856115471b
sandeepkota312 <sandeepkota312@gmail.com>
2024-09-14 20:29:25 +0530

    added migrations

--- userapp/models/scholarships.py
+++ userapp/models/scholarships.py
@@ -18,6 +18,7 @@ class ScholarshipData(BaseModel):
     deadline = models.DateField(null=True,default=None,blank=True)    #filtered by default
     link = models.URLField(null=True,default=None,blank=True)
     categories = models.ManyToManyField('Category', related_name='scholarships', blank=True)  #filter
+    is_approved = models.BooleanField(default=False)
     
     def __str__(self):
         return f"{self.id} - {self.title}"
