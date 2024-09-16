d88de846bcf2a4d716d1820869ef61fdfc852e1e
sandeepkota312 <sandeepkota312@gmail.com>
2024-09-16 23:34:05 +0530

    minor bug fixes

--- userapp/views/scholarships.py
+++ userapp/views/scholarships.py
@@ -8,7 +8,7 @@ from django.utils.timezone import now
 from userapp.models import ScholarshipData, UserScholarshipApplicationData, Category
 from userapp.serializers import ScholarshipDataSerializer, UserScholarshipDataSerializer, CategorySerializer
 from userapp.authentication import FirebaseAuthentication
-from userapp.permission import IsActivePermission,CanHostSites,IsActiveAndCanHostOrIsReviewer
+from userapp.permission import IsActivePermission,CanHostSites,IsActiveAndCanHostOrIsReviewer, IsVerfiedPermission
 from userapp.filters import ScholarshipDataFilter
 
 from rest_framework.authentication import SessionAuthentication
@@ -17,11 +17,11 @@ from rest_framework import viewsets
 from django.utils import timezone
 from django_filters.rest_framework import DjangoFilterBackend
 
-# DEFAULT_AUTH_CLASSES = [SessionAuthentication, FirebaseAuthentication]
+DEFAULT_AUTH_CLASSES = [SessionAuthentication, FirebaseAuthentication]
 
 
 class ScholarshipDataViewSet(viewsets.ModelViewSet):
-    queryset = ScholarshipData.objects.all()
+    queryset = ScholarshipData.objects.filter(is_approved=True)
     serializer_class = ScholarshipDataSerializer
     http_method_names = ["get","post","patch","delete"]
 
@@ -74,10 +74,12 @@ class ScholarshipDataViewSet(viewsets.ModelViewSet):
     
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
+        user = self.request.user
         if instance not in request.user.hostprofile.hosted_scholarships.all():
-            return Response(status=status.HTTP_401_UNAUTHORIZED)        
+            return Response(status=status.HTTP_401_UNAUTHORIZED)     
+        user.hostprofile.hosted_scholarships.remove(instance)  
         self.perform_destroy(instance)
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_202_ACCEPTED)
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -89,10 +91,25 @@ class UserScholarshipApplicationDataViewset(viewsets.ModelViewSet):
     queryset = UserScholarshipApplicationData.objects.all()
     serializer_class = UserScholarshipDataSerializer
     http_method_names = ["get", "post", "patch"]
-    # authentication_classes = DEFAULT_AUTH_CLASSES
-    # permission_classes = [IsActivePermission,IsVerfiedPermission]
+    authentication_classes = DEFAULT_AUTH_CLASSES
+    permission_classes = [IsActivePermission,IsVerfiedPermission]
 
     def get_queryset(self):
-        queryset = self.queryset.filter(user=self.request.user.id)
+        queryset = self.queryset.filter(user=self.request.user)
         return queryset
 
+    def create(self, request, *args, **kwargs):
+        user = request.user
+        scholarship_id = request.data.get('scholarship')
+
+        # Check if the application already exists for the user and scholarship
+        try:
+            application = UserScholarshipApplicationData.objects.get(user=user, scholarship_id=scholarship_id)
+            # Update the existing application based on the incoming data
+            serializer = self.get_serializer(application, data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            self.perform_update(serializer)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except UserScholarshipApplicationData.DoesNotExist:
+            # If the application does not exist, create a new one
+            return super().create(request, *args, **kwargs)
