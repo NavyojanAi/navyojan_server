10d4e834dd2291cf3bd53aeb9e2ebd45aa8e4643
sandeepkota312 <sandeepkota312@gmail.com>
2024-09-17 16:40:10 +0530

    admin search engine added

--- userapp/admin.py
+++ userapp/admin.py
@@ -2,7 +2,19 @@ from django.contrib import admin
 from userapp.models import UserScholarshipStatus,UserProfile, ScholarshipData, UserScholarshipApplicationData,Category,OTP,UserDocuments,UserPreferences,UserProfileScholarshipProvider
 # Register your models here.
 
-admin.site.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    # Adding fields to search
+    search_fields = ['user__email', 'user__first_name','user__username']
+
+    # Optional: Add filters for boolean fields in the right sidebar
+    list_filter = ['is_reviewer', 'is_host_user', 'free_account_privilages', 'premium_account_privilages']
+
+    # Display these fields in the list view for better visibility
+    list_display = ['user', 'is_reviewer', 'is_host_user', 'free_account_privilages', 'premium_account_privilages']
+
+# Registering the UserProfile model with the customized UserProfileAdmin
+admin.site.register(UserProfile, UserProfileAdmin)
+
 admin.site.register(ScholarshipData)
 admin.site.register(UserScholarshipApplicationData)
 admin.site.register(Category)
