1a372b216d27f87a3ba3fb3be603457cb5cddc81
divyej <divj32459@gmail.com>
2024-09-19 15:37:26 +0530

    headers

--- navyojan/settings.py
+++ navyojan/settings.py
@@ -117,6 +117,11 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8080",
    
 ]
+CORS_ALLOW_HEADERS = [
+    'authorization',
+    'content-type',
+    'x-refresh-token',  
+]
 ROOT_URLCONF = "navyojan.urls"
 
 
