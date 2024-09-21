8e681df512954bc0ddfe94a9a78e437c2c9114d9
divyej <divj32459@gmail.com>
2024-09-21 15:09:02 +0530

    dev

--- userapp/views/scholarships.py
+++ userapp/views/scholarships.py
@@ -20,7 +20,7 @@ DEFAULT_AUTH_CLASSES = [JWTAuthentication, FirebaseAuthentication]
 
 
 class ScholarshipDataViewSet(viewsets.ModelViewSet):
-    queryset = ScholarshipData.objects.filter(is_approved=True)
+    queryset = ScholarshipData.objects.filter(is_approved=True) 
     serializer_class = ScholarshipDataSerializer
     http_method_names = ["get","post","patch","delete"]
 
