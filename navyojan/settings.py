2e2e90a2d5cf6207d84d90959d7554756e54559b
sandeepkota312 <sandeepkota312@gmail.com>
2024-09-26 14:27:41 +0530

    addded hosts

--- navyojan/settings.py
+++ navyojan/settings.py
@@ -75,6 +75,8 @@ ALLOWED_HOSTS = [
     "http://navyojan.in",
     "http://www.navyojan.in",
     "http://www.navyojan.in/",
+    "http://navyojan.in/",
+    "http://navyojan.in"
     # Add any other hosts or domain names here
 ]
 
