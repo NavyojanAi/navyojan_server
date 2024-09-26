7722f28235a7f50728795e6765a3dd8aa9bbb9ab
sandeepkota312 <sandeepkota312@gmail.com>
2024-09-26 19:14:39 +0530

    changes

--- userapp/serializers/user.py
+++ userapp/serializers/user.py
@@ -65,7 +65,7 @@ class UserProfileScholarshipProviderSerializer(serializers.ModelSerializer):
     user = UserDisplaySerializer()
     class Meta:
         model = UserProfileScholarshipProvider
-        fields = "__all__"
+        fields = ["user","organisation", "org_site", "hosted_scholarships","can_host_scholarships"]
     
     def update(self, instance, validated_data):
         # Handle nested user update
